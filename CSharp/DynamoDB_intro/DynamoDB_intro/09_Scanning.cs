88ecd8eda71c8bd9a26d734f4be121cc2bb07cc8
Dongming Zhang <dzhang123@gmail.com>
2019-01-10 10:16:29 -0600

    CSharp version of getting started turorial

---
+++ CSharp/DynamoDB_intro/DynamoDB_intro/09_Scanning.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2.Model;
+using Amazon.DynamoDBv2.DocumentModel;
+
+namespace DynamoDB_intro
+{
+  public static partial class Ddb_Intro
+  {
+    /*--------------------------------------------------------------------------
+     *                             ClientScanning_async
+     *--------------------------------------------------------------------------*/
+    public static async Task<bool> ClientScanning_async( ScanRequest sRequest )
+    {
+      operationSucceeded = false;
+      operationFailed = false;
+
+      ScanResponse sResponse;
+      Task<ScanResponse> clientScan = client.ScanAsync(sRequest);
+      try
+      {
+        sResponse = await clientScan;
+      }
+      catch( Exception ex )
+      {
+        Console.WriteLine( "     -- FAILED to retrieve the movies, because:\n        {0}", ex.Message );
+        operationFailed = true;
+        pause( );
+        return( false );
+      }
+      Console.WriteLine( "     -- The low-level scan succeeded, and returned {0} movies!", sResponse.Items.Count );
+      if( !pause( ) )
+      {
+        operationFailed = true;
+        return ( false );
+      }
+
+      Console.WriteLine( "         Here are the movies retrieved:\n" +
+                         "         --------------------------------------------------------------------------" );
+      foreach( Dictionary<string, AttributeValue> item in sResponse.Items )
+        showMovieAttrsShort( item );
+
+      Console.WriteLine( "     -- Retrieved {0} movies.", sResponse.Items.Count );
+      operationSucceeded = true;
+      return ( true );
+    }
+  }
+}
---
+++ CSharp/DynamoDB_intro/DynamoDB_intro/10_DeletingTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DynamoDB_intro
+{
+  public static partial class Ddb_Intro
+  {
+    /*--------------------------------------------------------------------------
+     *                DeletingTable_async
+     *--------------------------------------------------------------------------*/
+    public static async Task<bool> DeletingTable_async( string tableName )
+    {
+      operationSucceeded = false;
+      operationFailed = false;
+
+      Console.WriteLine( "  -- Trying to delete the table named \"{0}\"...", tableName );
+      pause( );
+      Task tblDelete = client.DeleteTableAsync( tableName );
+      try
+      {
+        await tblDelete;
+      }
+      catch( Exception ex )
+      {
+        Console.WriteLine( "     ERROR: Failed to delete the table, because:\n            " + ex.Message );
+        operationFailed = true;
+        return ( false );
+      }
+      Console.WriteLine( "     -- Successfully deleted the table!" );
+      operationSucceeded = true;
+      pause( );
+      return ( true );
+    }
+  }
+}
